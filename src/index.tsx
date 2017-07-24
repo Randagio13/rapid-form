828838425974767e02be0da72c4f8c83a62845a7
Alessandro Casazza <imasterale@gmail.com>
2017-07-24 18:56:14 +0200

    Added default export for NPM

--- src/index.tsx
+++ src/index.tsx
@@ -10,36 +10,53 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 const cps = composeWithDevTools(applyMiddleware(thunk))
 const store = createStore(reducers, cps)
+const { env: { NODE_ENV } } = process
 
-const renderApp = (Component: any) => {
-  ReactDOM.render((
-    <AppContainer>
-      <Provider store={store}>
-        {Component}
-      </Provider>
-    </AppContainer>
-  ), document.getElementById('app'))
+if (NODE_ENV !== 'production') {
+  const renderApp = (Component: any) => {
+    ReactDOM.render((
+      <AppContainer>
+        <Provider store={store}>
+          {Component}
+        </Provider>
+      </AppContainer>
+    ), document.getElementById('app'))
+  }
+
+  const testOnSubmit = (event: any, data: object): void => {
+    event.preventDefault()
+    alert(JSON.stringify(data))
+  }
+
+  renderApp(
+    <FormContainer method='post' onSubmit={testOnSubmit} id='formId' theme='material-ui'>
+      <input type='text' name='username' label='Username' data-validation='empty' required />
+      <div className='testClass'>
+        <input type='password' name='password' label='Password' data-validation='empty'  />
+      </div>
+      <div className='testClass'>
+        <button type='submit' color='primary' raised>{'Sign up'}</button>
+      </div>
+    </FormContainer>
+  )
+
+  if (Reflect.get(module, 'hot') !== undefined) {
+    // tslint:disable-next-line:no-var-requires
+    const nextApp = require('./components').default
+    Reflect.get(module, 'hot').accept('./components', () => { renderApp(nextApp) })
+  }
 }
-
-const testOnSubmit = (event: any, data: object): void => {
-  event.preventDefault()
-  alert(JSON.stringify(data))
-}
-
-renderApp(
-  <FormContainer method='post' onSubmit={testOnSubmit} id='formId' theme='material-ui'>
-    <input type='text' name='username' label='Username' data-validation='empty' required />
-    <div className='testClass'>
-      <input type='password' name='password' label='Password' data-validation='empty' required />
-    </div>
-    <div className='testClass'>
-      <button type='submit' color='primary' raised>{'Sign up'}</button>
-    </div>
-  </FormContainer>
+/**
+ * Rapid Form Export
+ * @param { children, ...props } object
+ */
+const RapidForm = ({children, ...props}: any) => (
+  <AppContainer>
+    <Provider store={store}>
+      <FormContainer {...props}>
+        {children}
+      </FormContainer>
+    </Provider>
+  </AppContainer>
 )
-
-if (Reflect.get(module, 'hot') !== undefined) {
-  // tslint:disable-next-line:no-var-requires
-  const nextApp = require('./components').default
-  Reflect.get(module, 'hot').accept('./components', () => { renderApp(nextApp) })
-}
+export default RapidForm
