86cbfe4610b1f9617721c6937eceea8833c88253
Alessandro Casazza <imasterale@gmail.com>
2020-01-28 23:54:13 +0100

    feat(core): added types

--- src/hooks/useRapidForm.ts
+++ src/hooks/useRapidForm.ts
@@ -1,14 +1,29 @@
-import { useReducer, SyntheticEvent } from 'react'
+import { useReducer, SyntheticEvent, Ref } from 'react'
 import fetchReducer from '../utils/fetchReducer'
 import useValidation from './useValidation'
 import resetAll from '../utils/reset'
 import { resetInterface } from '../utils/reset'
 
-export interface SubmitCallback {
-  (data: object, errors: object, event?: SyntheticEvent<HTMLFormElement>): void
+export interface GeneralObject {
+  [key: string]: any
 }
 
-export default function useRapidForm(): any {
+export interface SubmitCallback {
+  (data: object, errors: object, event: SyntheticEvent<HTMLFormElement>): void
+}
+
+export interface useRapidFormInterface {
+  (): {
+    handleSubmit: (
+      c: SubmitCallback
+    ) => (e: SyntheticEvent<HTMLFormElement>) => void
+    errors: GeneralObject
+    validation: Ref<any>
+    reset: resetInterface
+  }
+}
+
+const useRapidForm: useRapidFormInterface = () => {
   const [state, dispatch] = useReducer(fetchReducer, {
     data: {},
     errors: {}
@@ -19,9 +34,7 @@ export default function useRapidForm(): any {
     resetAll(dispatch)
   }
   return {
-    handleSubmit: (c: SubmitCallback) => (
-      e: SyntheticEvent<HTMLFormElement>
-    ) => {
+    handleSubmit: c => e => {
       e.preventDefault()
       return c(state.data, state.errors, e)
     },
@@ -30,3 +43,5 @@ export default function useRapidForm(): any {
     reset
   }
 }
+
+export default useRapidForm
--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -3,15 +3,17 @@ import handleChange from '../utils/handleChange'
 import { ActionInterface } from '../utils/fetchReducer'
 
 export interface useValidationInterface {
-  (dispatch: Dispatch<ActionInterface>): void
+  (dispatch: Dispatch<ActionInterface>): null
 }
 
-const useValidation: useValidationInterface = dispatch =>
+const useValidation: useValidationInterface = dispatch => {
   useCallback(
     (ref: any): void => {
       ref.oninput = (e: React.ChangeEvent<any>) => handleChange(e, dispatch)
     },
     [dispatch]
   )
+  return null
+}
 
 export default useValidation
